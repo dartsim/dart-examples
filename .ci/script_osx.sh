fa3409449c67f15afecc6094d7a43d12e11b5459
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-05-23 19:23:36 -0400

    Update script_osx.sh

--- .ci/script_osx.sh
+++ .ci/script_osx.sh
@@ -4,14 +4,14 @@ cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j4
 cd ../..
 
-cp (brew --prefix dartsim6)/share/doc/dart/tutorials .
+cp $(brew --prefix dartsim6)/share/doc/dart/tutorials .
 cd .tutorials
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j4
 cd ../..
 
-cp (brew --prefix dartsim6)/share/doc/dart/examples .
+cp $(brew --prefix dartsim6)/share/doc/dart/examples .
 cd .examples
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
